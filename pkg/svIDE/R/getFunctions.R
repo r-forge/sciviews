1c6842f37c111702da3ed6b8a7a1f18e01a1cb7d
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2008-06-09 21:51:43 +0000

    Bug fixes in the DDE server in svIDE and little enhancement in svMisc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svIDE/R/getFunctions.R
+++ pkg/svIDE/R/getFunctions.R
@@ -1,15 +1,5 @@
 "getFunctions" <-
 function(pos) {
 	# Get a list of all R functions in a certain position
-	lst <- objects(pos = pos, all.names = TRUE)
-	l <- length(lst)
-	if (l == 0) return(NULL) else {
-		isFun <- rep(FALSE, l)
-		for (i in 1:l)
-			if (exists(lst[i], where = pos, mode = "function", inherits = FALSE))
-				isFun[i] <- TRUE
-		# Keep only functions
-		lst <- lst[isFun]
-		return(lst)
-	}
+	return(as.character(lsf.str(pos = pos, all.names = TRUE)))
 }
--- pkg/svMisc/R/Args.R
+++ pkg/svMisc/R/Args.R
@@ -1,8 +1,10 @@
 "Args" <-
 function(name, only.args = FALSE){
 	#### TODO: handle primitives and S3/S4 methods for generic functions
-	res <- eval(parse(text = paste("argsAnywhere(", name, ")", sep = "")))
-	if (is.null(res)) return("")	# Function 'name' not found
+	ret <- try(res <- eval(parse(text = paste("argsAnywhere(", name, ")",
+		sep = ""))), silent = TRUE)
+	if (inherits(ret, "try-error") || is.null(res))
+		return("")	# Function 'name' not found
 	res <- deparse(res)
 	res <- paste(res[-length(res)], collapse = "\n")
 	if (only.args) {
