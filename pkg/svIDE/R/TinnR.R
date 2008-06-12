e2baf2e42956a3eaeaf7b5c38ee6897a5fedb709
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-12 09:58:03 +0000

    Changes to Tinn-R object explorer functions in svIDE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svIDE/R/TinnR.R
+++ pkg/svIDE/R/TinnR.R
@@ -4,17 +4,62 @@
 # and objList() in the svMisc package
 
 "trObjSearch" <-
-function (path = NULL) {
-	return(invisible(objSearch(path = path, compare = FALSE)))
+function(path = NULL) {
+	res <- objSearch(sep = "\n", compare = FALSE)
+	if (is.null(path)) {
+		return(data.frame(search.. = strsplit(res, "\n")[[1]]))
+	} else {
+		cat("search..\n", res, "\n", sep = "", file = path)
+	}
 }
 
 "trObjList" <-
-function(id = "default", envir = .GlobalEnv, all.names = FALSE,
+function(id = "default", envir = ".GlobalEnv", all.names = TRUE,
 pattern = "", group = "", path = NULL) {
-	oWidth <- getOption("width")
-	options(width = 100)
-	on.exit(options(width = oWidth))
+	# Get data
+	res <- objList(id = id, envir = envir, all.names = all.names,
+		pattern = pattern, compare = FALSE)
 
-	return(invisible(objList(id = id, envir = envir, all.names = all.names,
-		pattern = pattern, group = group, path = path, compare = FALSE)))
+	if (length(res) == 1 && res == "") {
+		res <- data.frame("", "", "", "", stringsAsFactors = FALSE)
+	} else {
+		res <- data.frame(t(data.frame(strsplit(res, "\t"))),
+			stringsAsFactors = FALSE)[ , -5]
+	}
+	colnames(res) <- c("Name", "Dims", "Group", "Class")
+	rownames(res) <- NULL
+
+	# Group conversion
+	GrpTable <- c(
+		"vector",      "vector",    "vector",     "vector",
+		"vector",      "vector",    "vector",     "table",
+		"list",        "function",  "other",      "other",
+		"other",       "other")
+	names(GrpTable) <- c(
+		"numeric",     "complex",   "character",  "logical",
+		"factor",      "DateTime",  "raw",        "data.frame",
+		"list",        "function",  "NULL",       "language",
+		"S3",          "S4")
+	NewGroup <- GrpTable[res$Group]
+	NewGroup[NewGroup == "vector" & (regexpr("x", res$Dims) > -1)] <- "array"
+	NewGroup[NewGroup == "array" &
+		(regexpr("^[0-9]+x[0-9]+$", res$Dims) > -1)] <- "matrix"
+	res$Group <- NewGroup
+
+	# Filter according to group
+    if (group != "") {
+		if (group == "data") {
+			res <- res[NewGroup != "function", ]
+		} else {
+			res <- res[NewGroup == group, ]
+		}
+	}
+
+	# Final result
+	if (is.null(path)) {
+		return(res)
+	} else {
+		write.table(res, file = path, row.names = FALSE, quote = FALSE,
+			sep = "\t")
+	}
 }
