d678d4e457194343a877c471be0893085e39179c
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-06 13:07:40 +0000

    reworking of CompletePlus()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svMisc/R/CompletePlus.R
+++ pkg/svMisc/R/CompletePlus.R
@@ -1,160 +1,168 @@
-CompletePlus <- function (linebuffer, cursorPosition = nchar(linebuffer), minlength = 2, simplify = FALSE,
-  types = "arguments,functions,packages" ) {
-    
-    types <- strsplit( types, "," )[[1]]
-    
+CompletePlus <-
+function (linebuffer, cursorPosition = nchar(linebuffer), minlength = 2,
+simplify = FALSE, types = c("arguments", "functions", "packages")) {
     ### call the rcompgen API to get completions
     if (nchar(linebuffer, type = "chars") < minlength) return(invisible(NULL))
-    .assignLinebuffer(linebuffer)
-    .assignEnd(cursorPosition)
-    .guessTokenFromLine()
-    token <- .CompletionEnv[["token"]]
-    .completeToken()
-    comps <- .retrieveCompletions()
-    if ( !length(comps) ) return(invisible(NULL))
-    
+    utils:::.assignLinebuffer(linebuffer)
+    utils:::.assignEnd(cursorPosition)
+    utils:::.guessTokenFromLine()
+    token <- utils:::.CompletionEnv[["token"]]
+    utils:::.completeToken()
+    comps <- utils:::.retrieveCompletions()
+    if (!length(comps)) return(invisible(NULL))
+
     ### restrict the completion for which information is gathered (speed things up)
-    if( ! "arguments" %in% types ){
-      comps <- comps[ regexpr("=$", comps) < 0 ]
-    }
-    if ( !length(comps) ) return(invisible(NULL))
-    
-    if( ! "packages" %in% types ){
-      comps <- comps[ regexpr("::$", comps) < 0 ]
-    }
-    if ( !length(comps) ) return(invisible(NULL))
-    
-    if( ! "functions" %in% types ){
-      comps <- comps[ regexpr("(::|=)$", comps) > 0 ]
-    }
-    if ( !length(comps) ) return(invisible(NULL))
-    
+    if (!"arguments" %in% types)
+		comps <- comps[regexpr("=$", comps) < 0]
+    if (!length(comps))
+		return(invisible(NULL))
+
+    if (!"packages" %in% types)
+		comps <- comps[regexpr("::$", comps) < 0]
+    if (!length(comps))
+		return(invisible(NULL))
+
+    if (!"functions" %in% types)
+		comps <- comps[regexpr("(::|=)$", comps) > 0]
+    if (!length(comps))
+		return(invisible(NULL))
+
     ### build the output structure
-    out <- matrix( "", nrow = length(comps), ncol = 3 )
-    out[,1] <- comps
-    
-    # TODO: look at .win32consoleCompletion and figure out if the "additions" can be useful
-    
+    out <- matrix("", nrow = length(comps), ncol = 3)
+    out[, 1] <- comps
+
+    # TODO: look at utils:::.win32consoleCompletion and figure out if the
+	# "additions" can be useful
+
     ### deal with packages (completions ending with ::)
-    if(length(test.pack <- grep("::", comps) )){
-      out[ test.pack,3] <- sapply( sub("::", "", comps[test.pack]), packageDescription, fields = "Description"  ) 
-    }
-    
+    if (length(test.pack <- grep("::", comps)))
+		out[test.pack, 3] <- sapply(sub("::", "", comps[test.pack]),
+			packageDescription, fields = "Description")
+
     ### deal with argument completions (ending with =)
-    if(length(test.arg <- grep("=", comps) )){
-      arg <- sub("=$", "", comps[test.arg] )
-      fguess <- .CompletionEnv[["fguess"]]
-      pack <- sub( "^package:", "", find( fguess )[1] )
-      if(pack == ".GlobalEnv" ) {
-        out[ test.arg,3] <- ""
-      } else{
-        out[ test.arg,2] <- fguess
-        out[ test.arg,3] <- .extract_argument_description( pack, fguess, arg)
-      }
+    if (length(test.arg <- grep("=", comps))) {
+		arg <- sub("=$", "", comps[test.arg])
+		fguess <- utils:::.CompletionEnv[["fguess"]]
+		pack <- sub( "^package:", "", find(fguess)[1])
+		if(pack == ".GlobalEnv") {
+			out[test.arg, 3] <- ""
+		} else{
+			out[test.arg, 2] <- fguess
+			#out[test.arg, 3] <- .extract_argument_description(pack, fguess, arg)
+			out[test.arg, 3] <- descArgs(fguess, arg, pack)
+		}
     }
-    
+
     ### TODO: do not know what to do with these
     test.others <- grep(" ", comps)
     # TODO: are there other kind of completions I miss here
-    
+
     ### deal with function completions
-    test.fun <- setdiff( 1:length(comps), c(test.arg, test.pack, test.others) )
-    if( length(test.fun) ){
-      funs     <- comps[test.fun]
-      packs    <- sub( "^package:", "", sapply( funs , find ) )
-      desc.fun <- rep( "", length(packs) )
-      for( pack in unique(packs) ){
-        if( pack != ".GlobalEnv" ){
-          dir <- .find.package(pack)
-          rds <- file.path(dir, "Meta", "Rd.rds")
-          rd.data <- .readRDS(rds)
-          funInThisPack <- funs[ packs == pack ]
-          desc.fun[ packs == pack ] <- sapply( funInThisPack, function(x){
-            index <- which( sapply( rd.data$Aliases, function(y) any(x %in% y) ) )
-            rd.data$Title[ index ]
-          })
-        }
-      }
-      out[ test.fun, 2 ] <- packs
-      out[ test.fun, 3 ] <- desc.fun
+    test.fun <- setdiff(1:length(comps), c(test.arg, test.pack, test.others))
+    if (length(test.fun)) {
+		funs <- comps[test.fun]
+		packs <- sub("^package:", "", sapply(funs, find))
+		desc.fun <- rep("", length(packs))
+		for (pack in unique(packs)) {
+			if (pack != ".GlobalEnv") {
+				desc.fun[packs == pack] <- descFun(funs[packs == pack], pack)
+				#dir <- .find.package(pack)
+				#rds <- file.path(dir, "Meta", "Rd.rds")
+				#rd.data <- .readRDS(rds)
+				#funInThisPack <- funs[packs == pack]
+				#desc.fun[packs == pack] <- sapply(funInThisPack, function(x) {
+				#	index <- which(sapply(rd.data$Aliases, function(y)
+				#		any(x %in% y)))
+				#	rd.data$Title[index]
+				#})
+			}
+		}
+		out[test.fun, 2] <- packs
+		out[test.fun, 3] <- desc.fun
     }
-    
-    out[,3] <- gsub( "(\n|,)", " ", out[,3] )
-    
-    if( simplify ){
-      cat( apply( out, 1, paste, collapse="," ), sep = "\n" )
+
+    out[, 3] <- gsub("\t", "    ", out[, 3])
+	# Make sure that arguments are witten 'arg = ', and not 'arg='
+	out[, 1] <- sub("=$", " = ", out[, 1])
+
+    if (simplify) {
+		cat(apply(out, 1, paste, collapse = "\t"), sep = "\n")
     } else {
-      out
+		return(out)
     }
-    
 }
 
-.extract_argument_description <- function( package, fun, arg, lib.loc = NULL){  
-  dir <- .find.package(package, lib.loc)
-  rds <- file.path(dir, "Meta", "Rd.rds")
-  if (file_test("-f", rds)) {
-    rd.data <- .readRDS(rds)
-    index <- which( sapply( rd.data$Aliases, function(x) any(fun %in% x) ) )
-    if( length(index) == 1){
-      rdfile <- rd.data$File[index]
-      rddb <- Rd_db( package = package)
-      rdcontent <- paste( rddb[ basename(names(rddb)) == rdfile ][[1]], collapse = "\n" )
-      arguments <- .get_Rd_arguments_table( rdcontent )
-      arguments[ sapply( arg, function(x) which(arguments[,1] == x  )), 2 ]
-      # TODO: do some error handling
-    } else {
-      rep( "", length(arg) )
-    }
-  } else {
-    rep( "", length(arg))
-  }
-  
-}
-
-.get_Rd_arguments_table <-  function (txt) {
-    txt <- get_Rd_section(txt, "arguments")
-    tab <- get_Rd_items_table(txt)
-    txt <- tab[,1]
-    if (!length(txt)) 
-        return(character())
-    txt <- gsub("\\\\l?dots", "...", txt)
-    txt <- sub("^[[:space:]]+", "", txt)
-    txt <- sub("[[:space:]]+$", "", txt)
-    txt <- gsub("\\\\_", "_", txt)
-    tab[,1] <- txt
-    tab
-}
-
-### inspired from tools:::get_Rd_items
-### build a matrix where first column are the names of the arguments
-### and second column are the description
-### the function gets the information from the Rd help file
-get_Rd_items_table <- function (txt) {
-    out <- character() # item names
-    desc <- character() # item descriptions
-    
-    if (length(txt) != 1L) 
-        stop("argument 'txt' must be a character string")
-    pattern <- "(^|\n)[[:space:]]*\\\\item\\{"
-    while ((pos <- regexpr(pattern, txt)) != -1L) {
-        txt <- substring(txt, pos + attr(pos, "match.length") - 1L)
-        if ((pos <- delimMatch(txt)) == -1L) 
-            stop(gettextf("unmatched \\item name in '\\item{%s'", 
-                sub("\n.*$", "", txt)), domain = NA, call. = FALSE)
-        newout <- strsplit( substring(txt, pos + 1L, pos + attr(pos, "match.length") - 2L), " *, *" )[[1]]
-        out <- c(out, newout)
-        txt <- substring(txt, pos + attr(pos, "match.length"))
-        if ((pos <- regexpr("^[[:space:]]*\\{", txt)) == -1L) 
-            stop(gettextf("no \\item description for item '%s'", 
-                out[length(out)]), domain = NA, call. = FALSE)
-        txt <- substring(txt, pos + attr(pos, "match.length") - 1L)
-        if ((pos <- delimMatch(txt)) == -1L) 
-            stop(gettextf("unmatched \\item description for item '%s'", 
-                out[length(out)]), domain = NA, call. = FALSE)
-        desc <- c( desc, rep( substring(txt, 2L, pos + attr(pos, "match.length") - 2L), length(newout) ) )
-        txt <- substring(txt, pos + attr(pos, "match.length"))
-    }
-    cbind( out, desc )
-}
-
-
+## PhG: the following code is replaced by argsHelp() which is faster, and
+## do not depend on *experimental* code in the 'tools' package (limits also dependencies!)
+#.extract_argument_description <-
+#function (package, fun, arg, lib.loc = NULL) {
+#	dir <- .find.package(package, lib.loc)
+#	rds <- file.path(dir, "Meta", "Rd.rds")
+#	if (file_test("-f", rds)) {
+#		rd.data <- .readRDS(rds)
+#		index <- which(sapply(rd.data$Aliases, function(x) any(fun %in% x)))
+#		if (length(index) == 1) {
+#			rdfile <- rd.data$File[index]
+#			rddb <- Rd_db(package = package)
+#			rdcontent <- paste(rddb[basename(names(rddb)) == rdfile][[1]],
+#				collapse = "\n")
+#			arguments <- .get_Rd_arguments_table(rdcontent)
+#			arguments[sapply(arg, function(x) which(arguments[, 1] == x)), 2]
+#			# TODO: do some error handling
+#		} else {
+#			rep("", length(arg))
+#		}
+#	} else {
+#		rep("", length(arg))
+#	}
+#}
+#
+#.get_Rd_arguments_table <-
+#function (txt) {
+#    txt <- get_Rd_section(txt, "arguments")	# tools:::get_Rd_section
+#    tab <- get_Rd_items_table(txt)
+#    txt <- tab[, 1]
+#    if (!length(txt))
+#        return(character())
+#    txt <- gsub("\\\\l?dots", "...", txt)
+#    txt <- sub("^[[:space:]]+", "", txt)
+#    txt <- sub("[[:space:]]+$", "", txt)
+#    txt <- gsub("\\\\_", "_", txt)
+#    tab[, 1] <- txt
+#    tab
+#}
+#
+#### inspired from tools:::get_Rd_items
+#### build a matrix where first column are the names of the arguments
+#### and second column are the description
+#### the function gets the information from the Rd help file
+#get_Rd_items_table <-
+#function (txt) {
+#    out <- character() # item names
+#    desc <- character() # item descriptions
+#
+#    if (length(txt) != 1L)
+#        stop("argument 'txt' must be a character string")
+#    pattern <- "(^|\n)[[:space:]]*\\\\item\\{"
+#    while ((pos <- regexpr(pattern, txt)) != -1L) {
+#        txt <- substring(txt, pos + attr(pos, "match.length") - 1L)
+#        if ((pos <- delimMatch(txt)) == -1L)
+#            stop(gettextf("unmatched \\item name in '\\item{%s'",
+#                sub("\n.*$", "", txt)), domain = NA, call. = FALSE)
+#        newout <- strsplit(substring(txt, pos + 1L, pos +
+#			attr(pos, "match.length") - 2L), " *, *")[[1]]
+#        out <- c(out, newout)
+#        txt <- substring(txt, pos + attr(pos, "match.length"))
+#        if ((pos <- regexpr("^[[:space:]]*\\{", txt)) == -1L)
+#            stop(gettextf("no \\item description for item '%s'",
+#                out[length(out)]), domain = NA, call. = FALSE)
+#        txt <- substring(txt, pos + attr(pos, "match.length") - 1L)
+#        if ((pos <- delimMatch(txt)) == -1L)
+#            stop(gettextf("unmatched \\item description for item '%s'",
+#                out[length(out)]), domain = NA, call. = FALSE)
+#        desc <- c(desc, rep(substring(txt, 2L, pos + attr(pos, "match.length")
+#			- 2L), length(newout)))
+#        txt <- substring(txt, pos + attr(pos, "match.length"))
+#    }
+#    cbind(out, desc)
+#}
