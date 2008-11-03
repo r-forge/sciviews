d20410820e1c3c925381648b6afab6736fe57a82
romain <romain@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-03 11:36:23 +0000

    converting to integers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svTools/R/rdparse.R
+++ pkg/svTools/R/rdparse.R
@@ -7,8 +7,8 @@ rdparse <- function( file ){
 	names. <- sapply( chunks, function(x) x[1] %-~% "(^\\\\|{.*$)"  )
 	
 	cs <- cumsum( c(0, nchar( rdfile ) )  )
-	offset.start <- cs[ sapply( 1:max(index), function(i) which(index==i)[1] ) ]
-	offset.end   <- cs[ 1 + sapply( 1:max(index), function(i) tail( which(index==i), 1 ) ) ]
+	offset.start <- as.integer( cs[ sapply( 1:max(index), function(i) which(index==i)[1] ) ]             )
+	offset.end   <- as.integer( cs[ 1 + sapply( 1:max(index), function(i) tail( which(index==i), 1 ) ) ] )    
 	
 	list( offset.start = offset.start, 
 	  offset.end = offset.end, 
