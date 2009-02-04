1a3d33f5ac87b50524c7d962ccc9f85a1c7a7315
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-04 12:24:03 +0000

    replacement for language-specific context help (ko.help.language), allows "sv.r.help" to be run under common command.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ komodo/SciViews-K/content/js/r-help.js
@@ -0,0 +1,55 @@
+// this is replacement for language specific help on selection function
+// (cmd_helpLanguage command)
+// to handle R help not using system command
+
+// this should be keep updated with new versions of the original function
+ko.help.language = function () {
+    var language = null;
+    var view = ko.window.focusedView();
+    if (!view) {
+        view = ko.views.manager.currentView;
+    }
+    if (view != null) {
+        if (view.document) {
+            language = view.document.subLanguage;
+            if (language == "XML") {
+                language = view.document.language;
+            }
+        } else {
+            language = view.language;
+        }
+    }
+
+    var command = null, name = null;
+    if (language) {
+		//////////////////////// R help mod:
+		if (language == "R") {
+			var topic;
+			try {
+				topic = ko.interpolate.interpolateStrings("%w").trim();
+				//alert("*" + topic + "*")
+				sv.r.help(topic);
+				return;
+			} catch (e) {}
+
+		} //////////////// end: R help mod
+
+        if (gPrefs.hasStringPref(language + "HelpCommand")) {
+            command = gPrefs.getStringPref(language + "HelpCommand");
+        } else {
+            var langRegistrySvc = Components.classes['@activestate.com/koLanguageRegistryService;1'].getService(Components.interfaces.koILanguageRegistryService);
+            var languageObj = langRegistrySvc.getLanguage(language);
+            if (languageObj.searchURL) {
+                command = "%(browser) " + languageObj.searchURL;
+            }
+        }
+        if (command) {
+            name = language + " Help";
+        }
+    }
+    if (!command) {
+        command = gPrefs.getStringPref("DefaultHelpCommand");
+        name = "Help";
+    }
+    ko.run.runCommand(window, command, null, null, false, false, true, "no-console", 0, "", 0, name);
+}
