04884691526d468a1ad316e3783db59136cc9b4f
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-02 18:37:29 +0000

    Main overlay: added "start R" menu options allowing to choose preferred R application Several bugs on Linux/komodo4 fixed Typo in svMisc:Args corrected Added an xpi of current version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- komodo/SciViews-K/content/js/commands.js
+++ komodo/SciViews-K/content/js/commands.js
@@ -31,6 +31,67 @@ function _RControlSelection_supported() {
 	return(currentView.document.language == "R" && anythingSelected);
 }
 
+
+// selects the checkbox on selected element, while deselecting others
+this.getRApp = function(event) {
+	  var el = event.originalTarget;
+	  var siblings = el.parentNode.childNodes;
+	  for (var i = 0; i < siblings.length; i++) {
+			try {
+				  siblings[i].setAttribute("checked", siblings[i] == el);
+			} catch(e) {}
+	  }
+	  // this will preserve the selection:
+	  el.parentNode.setAttribute('selected',  el.id);
+	  // set the preference string:
+	  sv.prefs.setString("sciviews.preferredRApp", el.id, true);
+
+	  document.getElementById("cmd_svStartR").setAttribute("label",
+			sv.translate("StartR") + " (" + el.getAttribute('label') + ")");
+
+}
+
+
+// Selects checkbox on the 'preferred R application' menu on its first display
+// and hides unsupported commands
+// It is triggered on popupshowing event, onload would be better,
+// but it is for compatibility with komodo 4 which doesn't seem to support onload
+this.setMenuRApp = function (el) {
+	  var selected = el.getAttribute('selected');
+	  var siblings =  el.childNodes;
+
+	  var validPlatforms, showItem;
+	  var platform = navigator.platform;
+	  for (var i = 0; i < siblings.length; i++) {
+			try {
+				  validPlatforms = siblings[i].getAttribute("platform").split(/\s*[,\s]\s*/);
+				  showItem = false;
+				  for (var j in validPlatforms) {
+						if (platform.indexOf(validPlatforms[j]) == 0) {
+							  showItem = true;
+							  break;
+				  }}
+
+				  if  (!showItem) {
+						siblings[i].style.display = "none";
+				  } else {
+						siblings[i].setAttribute("checked",  siblings[i].id == selected);
+				  }
+
+			} catch(e) {}
+	  }
+
+	  // set the preference string:
+	  sv.prefs.setString("sciviews.preferredRApp", selected, false);
+
+	  document.getElementById("cmd_svStartR").setAttribute("label",
+			sv.translate("StartR") + " (" + document.getElementById(selected).getAttribute('label') + ")"
+			);
+
+	  // we do not need to run it anymore:
+	  el.removeAttribute("onpopupshowing");
+}
+
 this.openPkgManager = function() {
 	window.openDialog(
 	  "chrome://sciviewsk/content/pkgManager.xul",
@@ -38,7 +99,6 @@ this.openPkgManager = function() {
 	  "chrome=yes,dependent,centerscreen,resizable=yes,scrollbars=yes,status=no",
 	  sv);
 }
-
 
 this.setControllers = function() {
 	// make these commands active only when current document language is R
@@ -79,17 +139,55 @@ this.setControllers = function() {
 
 	// TODO: submenu with checkboxes to select preferred way to start R
 	viewManager.prototype.do_cmd_svStartR = function()	{
-		// runIn = "command-output-window", "new-console", "no-console"
+		// runIn = "command-output-window", "new-console",
 		//env strings: "ENV1=fooJ\nENV2=bar"
 		//gPrefSvc.prefs.getStringPref("runEnv");
 
+		var preferredRApp = sv.prefs.getString("sciviews.preferredRApp", "r-terminal");
+
+
 		var env = ["koPort=%(pref:sciviews.server.socket)", "koId=%(pref:sciviews.client.id)", "koHost=localhost",
-			 "koActivate=FALSE", "Rinitdir=~", "koServe=%(pref:sciviews.client.socket)", "Rid=Rgui"];
+			 "koActivate=FALSE", "Rinitdir=~", "koServe=%(pref:sciviews.client.socket)"];
 
 		var cwd = "%(path:hostUserDataDir)/XRE/extensions/sciviewsk@sciviews.org/templates";
-
 		cwd = ko.interpolate.interpolateStrings(cwd);
 
+
+		var command, mode = "no-console";
+
+		switch (preferredRApp) {
+			case "r-gui":
+				  env.push("Rid=Rgui");
+				  command = "Rgui --sdi";
+				  break;
+			case "r-xfce4-term":
+				  env.push("Rid=R-xfce4-term");
+				  command = "xfce4-terminal --title \"R\" -x R --quiet";
+				  break;
+			case "r-gnome-term":
+				  env.push("Rid=R-gnome-term");
+				  command = "gnome-terminal --hide-menubar --title=SciViews-R -x R --quiet";
+				  break;
+			case "r-kde-term":
+				  env.push("Rid=R-kde-term");
+				  command = "konsole --nomenubar --notabbar --noframe -T SciViews-R -e R --quiet";
+				  break;
+			case "r-tk":
+				  env.push("Rid=R-tk");
+				  env.push("DISPLAY=:0");
+				  command = "R --quiet --gui=Tk";
+				  mode = "new-console";
+				  break;
+			case "r-app":
+				  env.push("Rid=R.app");
+				  command = "open -a /Applications/R.app \"" + cwd + "\"";
+				  break;
+			default:
+				  env.push("Rid=R");
+				  command = "R --quiet";
+				  mode = "new-console";
+
+		}
 		for (i in env) {
 			try {
 				env[i] = ko.interpolate.interpolateStrings(env[i]);
@@ -98,13 +196,12 @@ this.setControllers = function() {
 				return;
 			}
 		}
-		ko.run.runCommand(window, "Rgui --sdi", cwd, env.join("\n"), false, false, false, "no-console", false, false, false)
+		ko.run.runCommand(window, command, cwd, env.join("\n"), false,
+						  false, false, mode, false, false, false);
 	};
 }
 
 }).apply(sv.command);
-
-
 
 
 //sv.cmdsSetControllers();
