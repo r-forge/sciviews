54cbea567891367d9c0f7b8374bbd6a97b39dfcf
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2009-06-05 09:25:43 +0000

    svUnit vignette in LyX 1.6 format + hyperref use in PDF changes to svSocket (first step to secure server)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@138 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svSocket/R/sendSocketServer.R
+++ pkg/svSocket/R/sendSocketServer.R
@@ -1,19 +1,19 @@
 "sendSocketServer" <-
-function (text, serverhost = "localhost", serverport = 8888, ...)
+function (text, host = "localhost", port = 8888, ...)
 {
 	# This function connects to a R socket server, evaluates a command in it
 	# get results and closes the connection. See example for a client that
 	# connects for a longer time and sends several commands to the server
 	if (!capabilities("sockets"))
 		stop("R must support socket connections")
-	con <- socketConnection(host = serverhost, port = serverport,
+	con <- socketConnection(host = host, port = port,
 		blocking = FALSE, ...)
 	on.exit(close(con))
 	# <<<q>>> tells the R socket server to close the connection once results
 	# are send back to the client
 	writeLines(paste("<<<q>>>", text, sep = ""), con)
 	res <- ""
-	while(regexpr("\n\f", res) < 0)
+	while(regexpr("\f", res) < 0)
 		res <- paste(res, readLines(con), sep = "", collapse = "\n")
-	return(sub("\n\f", "", res))
+	return(sub("\f", "", res))
 }
