101c1ffa6169b50770b0bc395f23a000ca0f97bf
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-27 23:00:07 +0000

    Addition of svSweave and tcltk2 R packages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@145 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/svSweave/R/cleanLyxRnw.R
@@ -0,0 +1,40 @@
+"cleanLyxRnw" <-
+function (RnwCon, RnwCon2 = RnwCon)
+{
+	# Read the data in the Rnw file
+	Rnw <- readLines(RnwCon)
+
+	# If the Rnw file is produced with LyX and SciViews Sweave module, chunks are
+	# separated by \Rchunk{<<[pars]>>= ... @}
+
+	# Beginning of R-Chunks (rewrite into <<[pars]>>=)
+	starts <- grepl("^\\\\Rchunk\\{<<.*>>=$", Rnw)
+	Rnw[starts] <- sub("^\\\\Rchunk\\{", "", Rnw[starts])
+
+	# End of R-Chunks (rewrite as @)
+	ends <- grepl("^@[ \t]*\\}$", Rnw)
+	Rnw[ends] <- "@"
+
+	parts <- cumsum(starts | ends)
+	chunk <- parts %% 2 > 0 	# R chunks are odd parts
+
+	# Do we need to change something?
+	if (!any(chunk))
+		return(writeLines(Rnw, RnwCon2))
+
+	# Eliminate empty strings not followed by empty strings inside chunks
+	Rnw[chunk & Rnw == "" & Rnw != c(Rnw[-1], " ")] <- NA
+	isna <- is.na(Rnw)
+	Rnw <- Rnw[!isna]
+	chunk <- chunk[!isna]
+
+	# Eliminate successive empty strings (keep only one)
+	Rnw[chunk & Rnw == "" & Rnw == c(Rnw[-1], " ")] <- NA
+	Rnw <- Rnw[!is.na(Rnw)]
+
+	# Convert tabulations into four spaces inside chunks (8 spaces by default)
+	Rnw[chunk] <- gsub("\t", "    ", Rnw[chunk])
+
+	# Write the result to the new .Rnw file
+	return(writeLines(Rnw, RnwCon2))
+}
