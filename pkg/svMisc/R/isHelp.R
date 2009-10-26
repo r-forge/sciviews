a78e5675791098970fe151d6b9f30600f159734f
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2009-10-26 07:01:29 +0000

    Temporary hack for index.search() in isHelp() and descFun() in svMisc package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@215 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svMisc/R/isHelp.R
+++ pkg/svMisc/R/isHelp.R
@@ -6,8 +6,16 @@ function (topic, package = NULL, lib.loc = NULL)
     if (!is.character(topic))
         topic <- deparse(topic)[1]
     INDICES <- .find.package(package, lib.loc, verbose = FALSE)
-    file <- index.search(topic, INDICES, "AnIndex", "R-ex")
-    # Neither help, nor example
+    # index.search() will not be visible any more and will have different
+	# arguments in R 2.11... and it is DEPRECATED in R 2.10
+	# => need to use a different code here!!!
+	# This is a temporary hack for svMisc to pass R CMD check on these versions
+	if (exists("index.search", mode= "function")) {
+		file <- index.search(topic, INDICES, "AnIndex", "R-ex")
+	} else {
+		file <- ""
+	}
+	# Neither help, nor example
 	if (file == "") return(c(help = FALSE, example = FALSE))
     packagePath <- dirname(dirname(file))
     if (length(file) > 1) {
