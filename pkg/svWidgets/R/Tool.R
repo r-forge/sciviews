7634269a04d57811647964f40b2ae4031de9ddad
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2010-09-25 09:44:18 +0000

    Addition of tkMenuItemCall() and a package man page Reworking style in all man pages and R code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@317 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svWidgets/R/Tool.R
+++ pkg/svWidgets/R/Tool.R
@@ -1,22 +1,18 @@
-"print.guiTool" <-
-function (x, ...)
+print.guiTool <- function (x, ...)
 {
 	cat("A SciViews GUI tool object:", "\n")
 	print(unclass(x))
 	return(invisible(x))
 }
 
-"ToolAdd" <-
-function (toolbar, side = "top")
+ToolAdd <- function (toolbar, side = "top")
 {
 	res <- switch(ToolType(toolbar),
-		tkTool = tkToolAdd(toolbar = toolbar, side = side)
-	)
+		tkTool = tkToolAdd(toolbar = toolbar, side = side))
 	return(invisible(res))
 }
 
-"ToolAddItem" <-
-function (toolbar, item, action, image = "", options = "")
+ToolAddItem <- function (toolbar, item, action, image = "", options = "")
 {
 	res <- switch(ToolType(toolbar),
 		tkTool = tkToolAddItem(toolbar = toolbar, item = item, action = action,
@@ -24,96 +20,87 @@ function (toolbar, item, action, image = "", options = "")
 	return(invisible(res))
 }
 
-"ToolDel" <-
-function (toolbar)
+ToolDel <- function (toolbar)
 {
     res <- switch(ToolType(toolbar),
 		tkTool = tkToolDel(toolbar = toolbar))
 	return(invisible(res))
 }
 
-"ToolDelItem" <-
-function (toolbar, item)
+ToolDelItem <- function (toolbar, item)
 {
     res <- switch(ToolType(toolbar),
 		tkTool = tkToolDelItem(toolbar = toolbar, item = item))
 	return(invisible(res))
 }
 
-"ToolNames" <-
-function ()
+ToolNames <- function ()
 {
 	res <- character(0)
-	# retrieve toolbar names from tk toolbars
+	## Retrieve toolbar names from tk toolbars
 	res <- c(res, names(getTemp(".guiTools")))
-	# eliminate toplevel entries
+	## Eliminate toplevel entries
     if (length(res) > 0) res <- res[regexpr("/", res) > 0]
 	return(res)
 }
 
-"ToolItems" <-
-function (toolbar)
+ToolItems <- function (toolbar)
 {
     res <- switch(ToolType(toolbar),
 		tkTool = tkToolItems(toolbar = toolbar))
 	return(res)
 }
 
-"ToolType" <-
-function (toolbar, warn = TRUE)
+ToolType <- function (toolbar, warn = TRUE)
 {
-	# Given a toolbar, return its type ("tkTool", NA)
+	## Given a toolbar, return its type ("tkTool", NA)
 	if (regexpr("^[$]Tk[.].+/", toolbar) > 0) return("tkTool") else {
         if (warn) warning("Unrecognized toolbar type for ", toolbar)
 		return(NA)
 	}
 }
 
-"ToolChangeItem" <-
-function (toolbar, item, action = "", options = "")
+ToolChangeItem <- function (toolbar, item, action = "", options = "")
 {
-	# Change action or options for toolbar entries
+	## Change action or options for toolbar entries
 	res <- switch(ToolType(toolbar),
 		tkTool = tkToolChangeItem(toolbar, item, action, options))
 	return(invisible(res))
 }
 
-"ToolStateItem" <-
-function (toolbar, item, active = TRUE)
+ToolStateItem <- function (toolbar, item, active = TRUE)
 {
-	# Activate/inactivate toolbar entries
+	## Activate/inactivate toolbar entries
 	res <- switch(ToolType(toolbar),
 		tkTool = tkToolStateItem(toolbar, item, active))
 	return(invisible(res))
 }
 
-"ToolInvoke" <-
-function (toolbar, item)
+ToolInvoke <- function (toolbar, item)
 {
-	# Invoke a toolbutton
+	## Invoke a toolbutton
 	res <- switch(ToolType(toolbar),
 		tkTool = tkToolInvoke(toolbar, item))
 	return(invisible(res))
 }
 
-"ToolRead" <-
-function (file = "Tools.txt")
+ToolRead <- function (file = "Tools.txt")
 {
-	# Read toolbars from a file
+	## Read toolbars from a file
     T <- scan(file, character(0), sep = "\n", comment.char = "#", quiet = TRUE)
-	# Split the lines into item, command, options
+	## Split the lines into item, command, options
     T <- strsplit(T, "~~")
-	# Strip leading and trailing spaces/tabs (used to align items in the file)
+	## Strip leading and trailing spaces/tabs (used to align items in the file)
     T <- lapply(T, function(x) sub("[ \t]+$", "", sub("^[ \t]+", "", x)))
-	# Move line after line and replace '|' by values
+	## Move line after line and replace '|' by values
     N <- length(T)
-	# Must have at least two entries
+	## Must have at least two entries
 	if (N < 2) return(invisible())
-	# First entry must be a toplevel, thus, it must start with '$'
+	## First entry must be a toplevel, thus, it must start with '$'
 	if (regexpr("^[$]", T[[1]][1]) < 0)
 		stop("first entry is not a toolbar!")
     toolLevels <- T[[1]][1]
-	# Initialize a data frame to contain decrypted info
+	## Initialize a data frame to contain decrypted info
 	dat <- rep("", N)
 	L <- data.frame(tool = I(dat), item = I(dat), image = I(dat),
 		action = I(dat), options = I(dat))
@@ -122,31 +109,31 @@ function (file = "Tools.txt")
 	L[1, ] <- Litem
 	for (i in 2:N) {
 		entry <- T[[i]][1]
-		# Split on '|'
+		## Split on '|'
 		split <- strsplit(entry, "[|]")[[1]]
-		# Combine toolLevels & split
+		## Combine toolLevels & split
 		last <- length(split)
 		toolLevels[last] <- split[last]
 		toolLevels <- toolLevels[1:last]
-		# Recombine toolLevels for getting recalculated entry
+		## Recombine toolLevels for getting recalculated entry
 		entry <- paste(toolLevels, collapse = "/")
-		# Is this just a tool button, or a menu tool button/menu item?
+		## Is this just a tool button, or a menu tool button/menu item?
 		lastentry <- basename(entry)
 		if (regexpr("^[$]", lastentry) > 0) { # This is a tool button
-			# Remove '$' before tool button entries
+			## Remove '$' before tool button entries
 			tool <- gsub("/[$]", "/", entry)
 			item <- ""      	# No item
 			image <- ""     	# No image
 			action <- if (last == 1) "[toolbar]" else "[tool]"
 			options <- ""       # No options (currently)
-		} else {    # This is an menu entry in a tool button menu
-			# tool = entry minus last item (remove '$' before tool entries)
+		} else {  # This is an menu entry in a tool button menu
+			## tool = entry minus last item (remove '$' before tool entries)
 			tool <- gsub("/[$]", "/", dirname(entry))
-			# Decrypt lastentry to get image & item ([image]item)
+			## Decrypt lastentry to get image & item ([image]item)
    			item <- sub("^[[][a-zA-Z0-9 ._-]+[]]", "", lastentry)
 			if (item == lastentry) image <- "" else {
 				image <- sub("^[[]([a-zA-Z0-9 ._-]+)[]].+$", "\\1", lastentry)
-				# Since these are Tk images resources, I have to prefix '$Tk.'
+				## Since these are Tk images resources, I have to prefix '$Tk.'
 				image <- paste("$Tk.", image, sep = "")
 			}
    			action <- T[[i]][2]
@@ -156,49 +143,47 @@ function (file = "Tools.txt")
 		}
         Litem <- data.frame(tool = I(tool), item = I(item), image = I(image),
 			action = I(action), options = I(options))
-		# add it to the data.frame
+		## Add it to the data.frame
 		L[i, ] <- Litem
 	}
 	
-	# The [toolbar] entries are not needed
+	## The [toolbar] entries are not needed
 	L <- subset( L, action != "[toolbar]" )
 
-	# Execute this line-by-line to create the various tools 
+	## Execute this line-by-line to create the various tools 
 	N <- nrow(L)
 	for (i in 1:N) {
 		action <- L$action[i]
 		if (action == "[tool]") { 	# Create a toolbar
             ToolAdd(toolbar = L$tool[i])
-#		} else if (action == "[tool]") { 	# Create a tool button
+#		} else if (action == "[tool]") {  # Create a tool button
 #			### TODO: determine which type of tool button it is!
 #			ToolAddItem(toolbar = L$menu[i])
 #		} else {    # Create a menu entry for a menu tool button
 #			MenuAddItem(menu = L$tool[i], item = L$item[i], action = L$action[i],
 #				image = L$image[i], options = L$options[i])
 #		}
-		} else { # Create a tool in the toolbar
+		} else {  # Create a tool in the toolbar
             ToolAddItem(tool = L$tool[i], item = L$item[i], action = L$action[i],
 				image = L$image[i], options = L$options[i])
 		}
 	}
 
-	# Return L invisibly
 	return(invisible(L))
 }
 
-"ToolReadPackage" <-
-function (package, subdir = "gui", file = "Tools.txt")
+ToolReadPackage <- function (package, subdir = "gui", file = "Tools.txt")
 {
-	# Create toolbars using a toolbar definition file located in a R package
+	## Create toolbars using a toolbar definition file located in a R package
 	dir <- system.file(subdir, package = package)
-	# Check that the dir exists
+	## Check that the dir exists
 	if (!file.exists(dir) || !file.info(dir)$isdir)
 		stop("'", dir, "' does not exist, or is not a directory!")
-	# Check that the file exists
+	## Check that the file exists
 	File <- file.path(dir, file)
 	if (!file.exists(File))
 		stop("'", file, "' not found in '", dir, "'!")
-	# Read the toolbar file
+	## Read the toolbar file
 	res <- ToolRead(File)
 	return(invisible(res))
 }
--- pkg/svWidgets/R/tkImg.R
+++ pkg/svWidgets/R/tkImg.R
@@ -1,26 +1,25 @@
-"tkImgAdd" <-
-function (file, type = "gif", update = FALSE)
+tkImgAdd <- function (file, type = "gif", update = FALSE)
 {
-	# Add a Tk image to the list (just GIF for the moment,
-	# but the Tcl/Tk Img package allows for more!)
+	## Add a Tk image to the list (just GIF for the moment,
+	## but the Tcl/Tk Img package allows for more!)
 	if (type != "gif")
 		stop("Only 'gif' images currently supported!")
 	if (!file.exists(file))
 		stop("File '", file, "' not found!")
-	# Load the image and assign it to an item to the .guiImgs object in TempEnv
+	## Load the image and assign it to an item to the .guiImgs object in TempEnv
 	.guiImgs <- getTemp(".guiImgs")
 	if (is.null(.guiImgs)) {
 		.guiImgs <- list()
 		class(.guiImgs) <- c("guiImg", "gui", class(.guiImgs))
 	}
-	# Calculate image name as being the basename of the file without extension
+	## Calculate image name as being the basename of the file without extension
 	Iname <- sub("[.][^.]+$", "", basename(file))
-	# In order to indicate it is a Tk resource, prepend '$Tk.'
+	## In order to indicate it is a Tk resource, prepend '$Tk.'
 	Iname <- paste("$Tk.", Iname, sep = "")
-	# If that name already exists, do nothing, except if we ask to update it
+	## If that name already exists, do nothing, except if we ask to update it
 	if (Iname %in% names(.guiImgs)) {
 		if (update) {
-			# Delete the image to recreate it with new resource
+			## Delete the image to recreate it with new resource
 			tcl("image", "delete", Iname)
 		} else return(invisible(Iname))	# Do nothing
 	}
@@ -28,37 +27,35 @@ function (file, type = "gif", update = FALSE)
 	tcl("image", "create", "photo", Image, file = file)
 
 	.guiImgs[[Iname]] <- Image
-	# Reassign .guiImgs to TempEnv
+	## Reassign .guiImgs to TempEnv
 	assignTemp(".guiImgs", .guiImgs)
 	return(invisible(Iname))
 }
 
-"tkImgDel" <-
-function (image)
+tkImgDel <- function (image)
 {
-	# Delete a tk image ressource from the list
+	## Delete a tk image ressource from the list
 	.guiImgs <- getTemp(".guiImgs")
-	# Is the image there?
+	## Is the image there?
 	if (!image %in% names(.guiImgs)) return(invisible(FALSE))
-	# Delete the image
+	## Delete the image
 	Image <- .guiImgs[[image]]
 	tcl("image", "delete", Image)
-	# Eliminate it from the list in .guiImgs
+	## Eliminate it from the list in .guiImgs
 	.guiImgs[[image]] <- NULL
-	# Reassign .guiImgs to TempEnv
+	## Reassign .guiImgs to TempEnv
 	assignTemp(".guiImgs", .guiImgs)
-	# Indicate that the image is actually deleted
+	## Indicate that the image is actually deleted
 	return(invisible(TRUE))
 }
 
-"tkImgRead" <-
-function (dir, type = "gif")
+tkImgRead <- function (dir, type = "gif")
 {
-	# Read all gif images from a directory into tkImage resources
-	# Check that the dir exists
+	## Read all gif images from a directory into tkImage resources
+	## Check that the dir exists
 	if (!file.exists(dir) || !file.info(dir)$isdir)
 		stop("'", dir, "' does not exist, or is not a directory!")
-	# List all file of 'type' in that directory
+	## List all file of 'type' in that directory
 	if (type != "gif")
 		stop("only type = 'gif' is currently supported")
 	pattern <- "[.][gG][iI][fF]$"
