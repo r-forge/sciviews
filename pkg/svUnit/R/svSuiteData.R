757ccf5701cfae927cf180cffd69fa4c6b4af1e5
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2010-10-31 10:58:56 +0000

    Slight code presentation changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@337 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svUnit/R/svSuiteData.R
+++ pkg/svUnit/R/svSuiteData.R
@@ -37,7 +37,7 @@ metadata <- function (object, ...)
 	UseMethod("metadata")
 
 metadata.svSuiteData <- function (object,
-	fields = c("R.version", "sessionInfo", "time", "description"), ...)
+fields = c("R.version", "sessionInfo", "time", "description"), ...)
 {
     ## Extract metadata information from a 'svSuiteData' object
 	if (!is.svSuiteData(object))
@@ -117,37 +117,34 @@ protocol_text.svSuiteData <- function (object, file = "", append = FALSE, ...)
 protocol_junit <- function (object, ...)
 	UseMethod("protocol_junit")
 
-protocol_junit.svSuiteData <- function (object, file="", append=FALSE, ...)
+protocol_junit.svSuiteData <- function (object, file = "", append = FALSE, ...)
 {
-  if (!is.svSuiteData(object))
-    stop("'object' must inherit from 'svSuiteData'")
-  if(!require(XML, quietly=TRUE))
-    return(invisible(FALSE))
+	if (!is.svSuiteData(object))
+		stop("'object' must inherit from 'svSuiteData'")
+	if(!require(XML, quietly = TRUE))
+		return(invisible(FALSE))
 
-  Tests <- sort(ls(object))
-  if((length(Tests) > 0) && inherits(object[[Tests[1]]], "svSuiteData"))
-    ## this is a set of suites (containing svSuiteData)
-    root <- xmlNode('testsuites')
-  else
-    ## this is a single suite (containing svTestData)
-    root <- xmlNode('testsuite')
+	Tests <- sort(ls(object))
+	if (length(Tests) > 0 && inherits(object[[Tests[1]]], "svSuiteData")) {
+		## This is a set of suites (containing svSuiteData)
+		root <- xmlNode('testsuites')
+	} else {
+		## This is a single suite (containing svTestData)
+		root <- xmlNode('testsuite')
+	}
 
-  with(stats(object), addAttributes(root, 
-                                    name=NULL,
-                                    tests=length(Tests),
-                                    errors=sum(kind == '**ERROR**'),
-                                    failures=sum(kind == '**FAILS**'),
-                                    skip=sum(kind == 'DEACTIVATED')))
-  if (length(Tests) > 0) {
-    for (Test in Tests)
-      root <- addChildren(root, kids=list(protocol_junit(object[[Test]], append=TRUE)))
-  }
+	with(stats(object), addAttributes(root, name = NULL, tests = length(Tests),
+		errors = sum(kind == '**ERROR**'), failures = sum(kind == '**FAILS**'),
+        skip = sum(kind == 'DEACTIVATED')))
+	for (Test in Tests)
+		root <- addChildren(root,
+			kids = list(protocol_junit(object[[Test]], append = TRUE)))
 
-  ## decide whether to return the xml node or write the xml file
-  if(append)
-    return(root)
-  else {
-    saveXML(root, file)
-    return(invisible(TRUE))
-  }
+	## Decide whether to return the xml node or write the xml file
+	if (isTRUE(append)) {
+		return(root)
+	} else {
+		saveXML(root, file)
+		return(invisible(TRUE))
+	}
 }
