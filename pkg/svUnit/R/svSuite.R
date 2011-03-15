e53010a52e43f8d4c3836795b1a55b24ffcc64be
mariotomo <mariotomo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-15 12:39:55 +0000

    solving ticket:1327 using a new local identifier to refer to the last test environment. the environment is stored globally in the same global identifier as before.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@365 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/svUnit/R/svSuite.R
+++ pkg/svUnit/R/svSuite.R
@@ -276,27 +276,28 @@ unitname = NULL, ...)
 	for (unit in names(.lastSuite)) {
 		## Create a new environment for this suite (created in .GlobalEnv so
 		## that we can inspect it in case of stop on error)
-		.TestSuiteEnv <<- new.env(parent = .GlobalEnv)
+		.ThisTestSuiteEnv <- new.env(parent = .GlobalEnv)
+        .TestSuiteEnv <<- .ThisTestSuiteEnv
 		## Source the corresponding file
 		Unit <- .lastSuite[[unit]]$file
-		sys.source(Unit, envir = .TestSuiteEnv)
+		sys.source(Unit, envir = .ThisTestSuiteEnv)
 		## Make sure there are .setUp() and .tearDown() functions
-		if (!exists(".setUp", envir = .TestSuiteEnv, mode = "function",
+		if (!exists(".setUp", envir = .ThisTestSuiteEnv, mode = "function",
 			inherits = FALSE))
-			.TestSuiteEnv$.setUp <- function() {}
-		if (!exists(".tearDown", envir = .TestSuiteEnv, mode = "function",
+			.ThisTestSuiteEnv$.setUp <- function() {}
+		if (!exists(".tearDown", envir = .ThisTestSuiteEnv, mode = "function",
 			inherits = FALSE))
-			.TestSuiteEnv$.tearDown <- function() {}
+			.ThisTestSuiteEnv$.tearDown <- function() {}
 		## List all test files in the unit
-		tests <- ls(.TestSuiteEnv, pattern = "^test.+$")
+		tests <- ls(.ThisTestSuiteEnv, pattern = "^test.+$")
 		## Keep only 'test*' objects that are function
 		keep <- unlist(lapply(tests, function(n) exists(n,
-			envir = .TestSuiteEnv, mode = "function", inherits = FALSE)))
+			envir = .ThisTestSuiteEnv, mode = "function", inherits = FALSE)))
 		tests <- tests[keep]
 		.Log$.lastSuite[[unit]]$tests <- tests
 		## Run each test in turn
 		for (test in tests) {
-			.runTest(envir = .TestSuiteEnv, test = test, unit = Unit)
+			.runTest(envir = .ThisTestSuiteEnv, test = test, unit = Unit)
 		}
 	}
 	return(invisible(files))
