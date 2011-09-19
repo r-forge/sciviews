f5bcfeae1330ac2243496a47f1c6d5caece72de3
mariotomo <mariotomo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-19 08:15:56 +0000

    ticket:1575
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@394 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/svUnit/R/runExamples.R
@@ -0,0 +1,26 @@
+##    This file is part of sciViews.
+##
+##    sciViews is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    sciViews is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with sciViews.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+makeTestListFromExamples <- function(packageName, manFilesDir) {
+  manPageFiles <- list.files(manFilesDir, pattern="\\.Rd$")
+  manPages <- substr(manPageFiles, 1, nchar(manPageFiles) - 3)
+  manPages <- manPages[manPages != paste(packageName, "package", sep="-")]
+
+  sapply(manPages, function(x) svTest(function() 
+                                      tryCatch(withCallingHandlers({ do.call(example, list(topic=x, package=packageName)); checkTrue(TRUE); },
+                                                                   warning=function(w) { checkIdentical(NULL, w) }),
+                                               error=function(w) checkIdentical(NULL, w))))
+}
