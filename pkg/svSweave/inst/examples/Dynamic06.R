8de48d8684b728e8d612f7f80fe5e66042d1c94a
phgrosjean <phgrosjean@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-06 22:26:18 +0000

    Many change to further integrate shiny into sweave/Asciidoc in svSweave
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sciviews@507 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/svSweave/inst/examples/Dynamic06.R
@@ -0,0 +1,68 @@
+#! /usr/bin/Rscript -e 'svSweave::RdocConvert()'
+svSweave::RdocHeader("Dynamic R document - Tabsets", toc = "none")
+!"
+<<server, eval = FALSE, echo = FALSE>>="
+## Define server logic for random distribution application
+dynamicServer({
+  
+	## Dynamic function to generate the requested distribution. This is 
+	## called whenever the inputs change. The output functions defined 
+	## below then all use the value computed from this function
+	data <- dynamic({  
+		dist <- switch(input$dist, norm = rnorm, unif = runif,
+            lnorm = rlnorm, exp = rexp, rnorm)
+    
+		dist(input$n)
+	})
+  
+	## Generate a plot of the data. Also uses the inputs to build the 
+	## plot label. Note that the dependencies on both the inputs and
+	## the data reactive function are both tracked, and all functions 
+	## are called in the sequence implied by the dependency graph
+	output$plot <- dynamicPlot({
+		dist <- input$dist
+		n <- input$n
+		hist(data(), main = paste("r", dist, "(", n, ")", sep = ""),
+			col = "cornsilk")
+	})
+  
+	## Generate a summary of the data
+	output$summary <- dynamicPrint({
+		summary(data())
+	})
+  
+	## Generate an HTML table view of the data
+	output$table <- dynamicTable({
+		data.frame(x = data())
+	})  
+})
+!"
+<<ui, echo = FALSE, results = ascii>>="
+## What to display in dynamic mode?
+asciiDynamic({
+	## Sidebar with controls to select the random distribution type
+	## and number of observations to generate. Note the use of the br()
+	## element to introduce extra vertical spacing
+	uiInput(
+		wiRadioButtons("dist", "Distribution type:",
+			c("Normal" = "norm", "Uniform" = "unif",
+			"Log-normal" = "lnorm", "Exponential" = "exp")),
+		br(),
+		wiSlider("n", "Number of observations:",
+			value = 500, min = 1, max = 1000)
+	)
+	
+	## # Show a tabset that includes a plot, summary, and table view
+	## of the generated distribution
+	uiTabsetPanel(
+		wiTabPanel("Plot", woPlot("plot")), 
+		wiTabPanel("Summary", woVerbatim("summary")), 
+		wiTabPanel("Table", woTable("table"))
+	)
+})
+## What to display in static mode?
+asciiStatic(uiP(strong(
+	"This application works only in dynamic mode through the shiny server!")))
+!"
+NOTE: //This is a reimplementation of **`06_tabsets'** shiny example as a **SciViews R script**//
+<<>>="
---
+++ pkg/svSweave/inst/examples/Dynamic08.R
@@ -0,0 +1,75 @@
+#! /usr/bin/Rscript -e 'svSweave::RdocConvert()'
+svSweave::RdocHeader("Dynamic R document - HTML UI", toc = "none")
+!"
+<<server, eval = FALSE, echo = FALSE>>="
+## Define server logic for random distribution application
+dynamicServer({
+	## Dynamic function to generate the requested distribution. This is 
+	## called whenever the inputs change. The output functions defined 
+	## below then all used the value computed from this function
+	data <- dynamic({  
+		dist <- switch(input$dist,
+            norm = rnorm,
+            unif = runif,
+            lnorm = rlnorm,
+			exp = rexp,
+            rnorm)
+    
+		dist(input$n)
+	})
+  
+	## Generate a plot of the data. Also uses the inputs to build the 
+	## plot label. Note that the dependencies on both the inputs and
+	## the data dynamic function are both tracked, and all functions 
+	## are called in the sequence implied by the dependency graph
+	output$plot <- dynamicPlot({
+		dist <- input$dist
+		n <- input$n
+    
+		hist(data(), main = paste("r", dist, "(", n, ")", sep = ""),
+			col = "cornsilk")
+	})
+  
+	## Generate a summary of the data
+	output$summary <- dynamicPrint({
+		summary(data())
+	})
+  
+	## Generate an HTML table view of the data
+	output$table <- dynamicTable({
+		data.frame(x = data())
+	})
+})
+!"
+<<ui, echo = FALSE, results = ascii>>="
+## We can completely define the UI in HTML inside a passthrough block
+## This allows us the see how input and output widgets are defined in the page
+cat('
+[pass]
+++++++++++
+	<p>
+		<label>Distribution type:</label><br />
+		<select name="dist">
+			<option value="norm">Normal</option>
+			<option value="unif">Uniform</option>
+			<option value="lnorm">Log-normal</option>
+			<option value="exp">Exponential</option>
+		</select> 
+	</p>
+ 
+	<p>
+		<label>Number of observations:</label><br /> 
+		<input type="number" name="n" value="500" min="1" max="1000" />
+	</p>
+ 
+	<pre id="summary" class="shiny-text-output"></pre> 
+  
+	<div id="plot" class="shiny-plot-output" 
+		style="width: 100%; height: 400px"></div> 
+	
+	<div id="table" class="shiny-html-output"></div>
++++++++++++
+')
+!"
+NOTE: //This is a reimplementation of **`08_html'** shiny example as a **SciViews R script**.//
+<<>>="
